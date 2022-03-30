57c8d53144bb4337d858862b4750f74564d13304
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-30 11:35:42 -0700

    Add files via upload

---
+++ Access Modifiers/src/package1/A.java
@@ -0,0 +1,15 @@
+package package1;
+import package2.*;
+
+public class A {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		B b = new B();
+		b.privateMessage();
+
+	}
+
+}
---
+++ Access Modifiers/src/package1/B.java
@@ -0,0 +1,14 @@
+package package1;
+import package2.*;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		C c = new C();
+		System.out.println(c.publicMessage);
+
+	}
+	
+}
